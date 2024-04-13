41412dee8151939e1f1e3011f8836fc89ff717dc
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-04-13 12:27:24 -0400

    Bow
    I got a working 2d bow and arrow in the game and you can shoot it

---
+++ Scripts/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var bow_equipped = true
+var bow_cooldown = true
+var arrow = preload("res://Scenes/arrow.tscn")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	#move_and_slide()
+	
+	#Arrow shooting script
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("shoot_arrow") and bow_equipped and bow_cooldown:
+		bow_cooldown = false
+		var arrow_instance = arrow.instantiate()
+		arrow_instance.rotation = $Marker2D.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		add_child(arrow_instance)
+
+		await get_tree().create_timer(0.4).timeout
+		bow_cooldown = true
+
