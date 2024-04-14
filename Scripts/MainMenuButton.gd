6243efef1ee2c93e693b5eaf05389619ba2d4cbb
egguterra <allisonrmj@gmail.com>
2024-04-13 20:55:08 -0400

    added sound effect to main menu button

--- Scripts/MainMenuButton.gd
+++ Scripts/MainMenuButton.gd
@@ -1,5 +1,11 @@
 extends Button
 
+@export var buttonsound: AudioStreamPlayer
+@export var buttondelay: Timer
 
 func _on_pressed():
+	buttonsound.play()
+	buttondelay.start()
+	
+func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://Scenes/start_scene.tscn")
