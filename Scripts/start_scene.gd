d46744dfe2376ab4a5b3e2e0fe499860536520bf
egguterra <allisonrmj@gmail.com>
2024-04-13 20:49:40 -0400

    Added click sound to Play button

--- Scripts/start_scene.gd
+++ Scripts/start_scene.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@export var buttonsound: AudioStreamPlayer
+@export var buttondelay: Timer
+
 func _on_quit_button_pressed():
 	get_tree().quit()
 
 
 
 func _on_start_button_pressed():
+	buttonsound.play()
+	buttondelay.start()
+
+func _on_buttondelay_timeout():
 	get_tree().change_scene_to_file("res://Scenes/levelOne.tscn")
