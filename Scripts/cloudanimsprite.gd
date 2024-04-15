e403c5ec31389b8c9e2ada254f109ed904a2caee
bluupers <blupineg@iu.edu>
2024-04-15 17:14:15 -0400

    clouds??? mostly???

---
+++ Scripts/cloudanimsprite.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+#var cloudType = ["bumpy", "flat", "regular"]
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#var c = int(randi_range(0,2))
+	#set_frame(c)
+	
+	
+	
--- Scripts/levelOne.gd
+++ Scripts/levelOne.gd
@@ -2,24 +2,35 @@ extends Node2D
 const scroll_speed: int = 1
 var screen_size: Vector2i
 @export var cloudscene : PackedScene
+#@export var cloudanim : AnimatedSprite2D
 const cloud_delay : int = 100
 const cloud_range : int = 200
 var clouds: Array
 
+
 func _ready():
 	screen_size = get_window().size
+	new_game()
 	
-#func _process(delta):
-	#for cloud in clouds: 
-		#cloud.position.x -= scroll_speed
-	
-func _on_cloud_timer_timeout():
+func new_game(): 
+	clouds.clear()
 	generate_clouds()
+	$cloudtimer.start()
 	
+func _process(delta):
+	for cloud in clouds: 
+		cloud.position.x -= scroll_speed
+	
+func _on_cloudtimer_timeout():
+	generate_clouds()
+
 func generate_clouds():
 	var cloud = cloudscene.instantiate()
-	#cloud.position.x = screen_size.x 
-	#cloud.position.y = screen_size.y / 2 + randi_range(-cloud_range, cloud_range)
+	
+	cloud.position.x = screen_size.x + cloud_delay
+	cloud.position.y = randi_range(0, 1000)
 	add_child(cloud)
 	clouds.append(cloud)
-	cloud.get_node("cloudtimer").start()
+
+
+
