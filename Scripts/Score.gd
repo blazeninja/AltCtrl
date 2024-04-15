a5ef31e10bf0cbb2048c4270fd10ee132d96ed73
egguterra <allisonrmj@gmail.com>
2024-04-15 12:56:19 -0400

    Added Arrows UI to level one

--- Scripts/Score.gd
+++ Scripts/Score.gd
@@ -1,17 +1,1 @@
 extends Label
-
-
-# Called when the node enters the scene tree for the first time.
-#@onready var score = $"/root/Score"
-#var score = 0
-func _ready():
-	#goto autoloasd
-	#get score
-	#add score to text
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	self.text = str(Global.score)
-	pass
