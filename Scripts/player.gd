0492c56c032a11d0d4604d425668423defe414e8
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-04-15 01:40:04 -0400

    Timer
    Got a timer working so that we can do a arrow shoot power mechanic.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,6 +14,12 @@ func _physics_process(delta):
 	var mouse_pos = get_global_mouse_position()
 	$Marker2D.look_at(mouse_pos)
 	
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	if Input.is_action_just_pressed("shoot_arrow"):
+		$Timer.start(1)
+	
 	if Input.is_action_just_released("shoot_arrow") and bow_equipped and bow_cooldown and arrow_count >= 1:
 		bow_cooldown = false
 		var arrow_instance = arrow.instantiate()
@@ -21,6 +27,7 @@ func _physics_process(delta):
 		arrow_instance.global_position = $Marker2D.global_position
 		add_child(arrow_instance)
 		arrow_count -= 1
+		print($Timer.time_left)
 
 		await get_tree().create_timer(0.4).timeout
 		bow_cooldown = true
