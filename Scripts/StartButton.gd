b56d31ef2cc8cbd25dd7e8d4fda2c408e2f552ae
egguterra <allisonrmj@gmail.com>
2024-04-22 12:37:32 -0400

    made arrow count set to 10 when you start

--- Scripts/StartButton.gd
+++ Scripts/StartButton.gd
@@ -6,6 +6,7 @@ extends Button
 func _on_pressed():
 	buttonsound.play()
 	buttondelay.start()
+	get_node("/root/Global").arrowCount = 10
 	
 func _on_buttondelay_timeout():
 	get_tree().change_scene_to_file("res://Scenes/levelOne.tscn")
