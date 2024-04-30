aa967ab491c255fc6e3fc9f60d372b9dd0818cd5
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-04-29 21:56:09 -0400

    Level 2

---
+++ Scripts/arrowPower.gd
@@ -0,0 +1,14 @@
+extends ProgressBar
+
+var time_left = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#time_left = time_left - delta
+	#value = floor(time_left)
+	pass
--- Scripts/levelTwo.gd
+++ Scripts/levelTwo.gd
@@ -3,4 +3,5 @@ extends Node2D
 signal arrow_count_changed(count)
 
 var arrow_count = 10
+@onready var target = $TargetProto
 
